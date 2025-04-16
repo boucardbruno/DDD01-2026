e041720dd821a9410bee9fad35505858432dba05
Bruno Boucard <boucard.bruno@free.fr>
2025-04-16 08:28:57 +0200

    refactoring names (too long)

---
+++ TheaterSuggestions/Java/ScreeningSeatingArrangementSuggestions.Api/src/main/java/org/octo/SeatingArrangementApi/controllers/SeatingArrangementRecommenderSuggestions.java
@@ -0,0 +1,30 @@
+package org.octo.SeatingArrangementApi.controllers;
+
+import org.octo.seatingplacessuggestions.domain.seatingplacesuggestions.PartyRequested;
+import org.octo.seatingplacessuggestions.domain.seatingplacesuggestions.ShowID;
+import org.octo.seatingplacessuggestions.domain.seatingplacesuggestions.SuggestionsAreMade;
+
+import org.octo.seatingplacessuggestions.domain.seatingplacesuggestions.port.ISeatingArrangementRecommenderSuggestions;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/SeatingArrangementRecommenderSuggestions")
+public class SeatingArrangementRecommenderSuggestions {
+
+    private final ISeatingArrangementRecommenderSuggestions seatingArrangementRecommenderSuggestions;
+
+    public SeatingArrangementRecommenderSuggestions(ISeatingArrangementRecommenderSuggestions seatingArrangementRecommenderSuggestions) {
+        this.seatingArrangementRecommenderSuggestions = seatingArrangementRecommenderSuggestions;
+    }
+
+    // GET api/SeatingArrangementRecommenderSuggestion?showId=5&party=3
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<SuggestionsAreMade> makeSuggestions(@RequestParam String showId, @RequestParam int party) {
+        var suggestionsMade = seatingArrangementRecommenderSuggestions.makeSuggestions(new ShowID(showId),new PartyRequested(party));
+        return ResponseEntity.ok(suggestionsMade);
+    }
+}
