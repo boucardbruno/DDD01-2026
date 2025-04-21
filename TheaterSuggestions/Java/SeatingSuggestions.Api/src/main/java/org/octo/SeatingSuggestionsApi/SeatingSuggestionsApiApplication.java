6d546c5f90da0222c4f2b1cc5d50f9d346740ce4
Bruno Boucard <boucard.bruno@free.fr>
2025-04-21 15:51:23 +0200

    Swagger enabled

--- TheaterSuggestions/Java/SeatingSuggestions.Api/src/main/java/org/octo/SeatingSuggestionsApi/SeatingSuggestionsApiApplication.java
+++ TheaterSuggestions/Java/SeatingSuggestions.Api/src/main/java/org/octo/SeatingSuggestionsApi/SeatingSuggestionsApiApplication.java
@@ -3,6 +3,7 @@ package org.octo.SeatingSuggestionsApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// swagger : http://localhost:8080/swagger-ui/index.html
 @SpringBootApplication
 public class SeatingSuggestionsApiApplication {
     public static void main(String[] args) {
