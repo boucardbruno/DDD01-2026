8b51a482159417c38abea7e62d195727385ee0a9
Bruno Boucard <boucard.bruno@free.fr>
2025-04-25 10:49:42 +0200

    if there is no suggestion, we should return 200 and not 400

--- TheaterSuggestions/Java/ScreeningSeatingArrangementSuggestionsAcceptanceTests/src/test/java/org/octo/seatssuggestionsacceptancetests/SeatingArrangementRecommenderTest.java
+++ TheaterSuggestions/Java/ScreeningSeatingArrangementSuggestionsAcceptanceTests/src/test/java/org/octo/seatssuggestionsacceptancetests/SeatingArrangementRecommenderTest.java
@@ -49,52 +49,6 @@ class SeatingArrangementRecommenderTest {/*
 
         assertThat(suggestionsAreMade).isInstanceOf(SuggestionsAreAreNotAvailable.class);
     }
-
-    @Test
-    void suggest_two_seatingPlaces_when_Auditorium_contains_all_available_seatingPlaces() throws IOException {
-        // Lincoln-17
-        //
-        //     1   2   3   4   5   6   7   8   9  10
-        //  A: 2   2   1   1   1   1   1   1   2   2
-        //  B: 2   2   1   1   1   1   1   1   2   2
-        final String showId = "17";
-        final int partyRequested = 2;
-
-        var auditoriumSeatingArrangements =
-                new AuditoriumSeatingArrangements(new AuditoriumLayoutRepository(), new ReservationsProvider());
-
-        // Make this assertion real to the expected one with outcomes:
-        var seatingArrangementRecommender = new SeatingArrangementRecommender(auditoriumSeatingArrangements);
-        var suggestionsAreMade = seatingArrangementRecommender.makeSuggestion(showId, partyRequested);
-
-        assertThat(suggestionsAreMade.seatNames(PricingCategory.SECOND)).containsExactly("A1", "A2", "A9", "A10", "B1", "B2");
-
-    }
-
-   @Test
-   void suggest_three_availabilities_per_PricingCategory() throws IOException {
-        // New Amsterdam-18
-        //     1   2   3   4   5   6   7   8   9  10
-        //  A: 2   2   1   1   1   1   1   1   2   2
-        //  B: 2   2   1   1   1   1   1   1   2   2
-        //  C: 2   2   2   2   2   2   2   2   2   2
-        //  D: 2   2   2   2   2   2   2   2   2   2
-        //  E: 3   3   3   3   3   3   3   3   3   3
-        //  F: 3   3   3   3   3   3   3   3   3   3
-        final String showId = "18";
-        final int partyRequested = 1;
-
-       var auditoriumSeatingArrangements =
-               new AuditoriumSeatingArrangements(new AuditoriumLayoutRepository(), new ReservationsProvider());
-
-       // Make this assertion real to the expected one with outcomes:
-       var seatingArrangementRecommender = new SeatingArrangementRecommender(auditoriumSeatingArrangements);
-       var suggestionsAreMade = seatingArrangementRecommender.makeSuggestion(showId, partyRequested);
-
-       assertThat(suggestionsAreMade.seatNames(PricingCategory.FIRST)).containsExactly("A3","A4","A5");
-       assertThat(suggestionsAreMade.seatNames(PricingCategory.SECOND)).containsExactly("A1", "A2", "A9");
-       assertThat(suggestionsAreMade.seatNames(PricingCategory.THIRD)).containsExactly("E1", "E2", "E3");
-    }
 }
 
 
