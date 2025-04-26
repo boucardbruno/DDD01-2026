0a4a0af957770f9fc5d77c470d735f9f4499e76b
Bruno Boucard <boucard.bruno@free.fr>
2025-04-26 10:25:04 +0200

    add a few comments to help attendees

--- TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SeatingArrangementRecommender.cs
+++ TheaterSuggestions/CSharp/TheaterSuggestions.Tests/SeatingArrangementRecommender.cs
@@ -4,11 +4,12 @@ public class SeatingArrangementRecommender
 {
     public SeatingArrangementRecommender(AuditoriumSeatingArrangements auditoriumSeatingArrangements)
     {
-         
+        // TODO: Implement the constructor logic to initialize the recommender with the auditorium seating arrangements.
     }
 
     public SuggestionsAreMade MakeSuggestions(string showId, int partyRequested)
     {
+        // TODO: Implement the logic to suggest seating arrangements based on the showId and partyRequested.
         return new SuggestionsAreMade(showId, partyRequested);
     }
 }
--- TheaterSuggestions/Java/ScreeningSeatingArrangementSuggestionsAcceptanceTests/src/test/java/org/octo/seatssuggestionsacceptancetests/SeatingArrangementRecommender.java
+++ TheaterSuggestions/Java/ScreeningSeatingArrangementSuggestionsAcceptanceTests/src/test/java/org/octo/seatssuggestionsacceptancetests/SeatingArrangementRecommender.java
@@ -2,9 +2,11 @@ package org.octo.seatssuggestionsacceptancetests;
 
 public class SeatingArrangementRecommender {
     public SeatingArrangementRecommender(AuditoriumSeatingArrangements auditoriumSeatingArrangements) {
+        // TODO: Implement the constructor logic to initialize the recommender with the auditorium seating arrangements.
     }
 
     public SuggestionsAreMade makeSuggestion(String showId, int partyRequested) {
+        // TODO: Implement the logic to suggest seating arrangements based on the showId and partyRequested.
         return new SuggestionsAreMade(showId, partyRequested);
     }
 }
